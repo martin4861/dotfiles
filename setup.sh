f96028f5b283c64ffed0d7d6c7d877a17d07b773
martin4861 <martin4861@gmx.de>
2021-07-14 13:12:59 +0200

    Added fasd install call

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 sudo apt-get install thefuck
 
+git clone https://github.com/clvv/fasd.git ~/fasd && cd ~/fasd && sudo make install
+
 ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/p10k.zsh ~/.p10k.zsh
