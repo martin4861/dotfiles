de87624f89ae27093bbe57ad162c0df52996fed4
martin4861 <martin4861@gmx.de>
2021-07-14 21:46:37 +0200

    Added tmux config

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ git clone https://github.com/clvv/fasd.git ~/fasd && cd ~/fasd && sudo make inst
 ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/p10k.zsh ~/.p10k.zsh
+
+
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
